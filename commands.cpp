ad264fe39c102d51c703310451047dcac506f471
Jonas Zinn <Jonas.Zinn@uni-konstanz.de>
2016-03-16 18:18:20 +0100

    test

--- commands.cpp
+++ commands.cpp
@@ -10,21 +10,6 @@
 int update(int argc, vector<string> argv, string from)
 {
 
-  char mystring [100];
-  FILE *command = popen("git --work-tree=/home/jonas/Develop/Chatty --git-dir=/home/jonas/Develop/Chatty/.git pull", "r");
-
-  while(fgets (mystring , 100 , command) != NULL)
-    cout << mystring;
-
-  pclose(command);
-
-  command = popen("cmake .. && make", "r");
-
-  while(fgets (mystring , 100 , command) != NULL)
-    cout << mystring;
-
-  pclose(command);
-
   char path[256];
   char dest[256];
   pid_t pid = getpid();
@@ -36,7 +21,23 @@ int update(int argc, vector<string> argv, string from)
   args[0] = dest;
   args[1] = NULL;
 
+  char mystring [100];
+  FILE *command = popen("git --work-tree=/home/jonas/Develop/Chatty --git-dir=/home/jonas/Develop/Chatty/.git pull", "r");
+
+  while(fgets (mystring , 100 , command) != NULL)
+    cout << mystring;
+
+  pclose(command);
+
+
   unlink(dest);
+
+  command = popen("cmake .. && make", "r");
+
+  while(fgets (mystring , 100 , command) != NULL)
+    cout << mystring;
+
+  pclose(command);
 
   if( execv(dest, args) == -1)
   {
